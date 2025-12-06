178b63fbc44a2acf0f10ee6f6851dffd871e324f
solafidezamili12345 <solafidezamili1945@gmail.com>
2025-12-06 13:56:44 +0700

    Mengupdate

--- app/page.tsx
+++ app/page.tsx
@@ -20,24 +20,24 @@ export default function Home() {
         <h2 className={styles.title}>ABOUT —</h2>
         <p className={styles.aboutText}>
           A fresh graduate in Informatics Engineering, 
-          passionate about Web development, IoT system and Deep Learning Enthusiast
+          passionate about Web development, IoT system and Deep Learning Enthusiast.
         </p>
 
         {/* Wrapper untuk info detail (kita buat 2 kolom) */}
         <div className={styles.aboutDetails}>
           <ul>
-            <li><strong>Birthday:</strong> 30. May. 2005</li>
-            <li><strong>City:</strong> Jakarta</li>
-            <li><strong>Study:</strong> Sumatera Institute of Technology</li>
+            {/* <li><strong>Birthday:</strong> 30. May. 2005</li> */}
+            {/* <li><strong>Study:</strong> Sumatera Institute of Technology</li> */}
             <li><strong>E-mail:</strong> solafidezamili1945@gmail.com</li>
             <li><strong>Phone:</strong> +6282260025752</li>
+            <li><strong>Interest:</strong> Web & IoT Development, with Deep Learning Enthusiast</li>
           </ul>
           <ul>
-            <li><strong>Degree:</strong> Honors</li>
+            {/* <li><strong>Degree:</strong> Honors</li> */}
+            <li><strong>Regency:</strong> Tangerang, Banten</li>
             <li><strong>Country:</strong> Indonesia</li>
-            <li><strong>Interest:</strong> Web & IoT Development, with Deep Learning Enthusiast</li>
             <li><strong>Lang:</strong> English</li>
-            <li><strong>Website:</strong> portfolio-zamili-app.vercel.app</li>
+            {/* <li><strong>Website:</strong> portfolio-zamili-app.vercel.app</li> */}
           </ul>
         </div>
       </section>
@@ -53,22 +53,22 @@ export default function Home() {
           <div className={styles.serviceCard}>
             {/* Ganti [ICON] dengan komponen ikon nanti */}
             <h3><FaDev /> WEB DEVELOPER</h3> 
-            <p>Lorem ipsum dolor sit amet, tur adipisicing elit tempor.</p>
+            <p>Building responsive websites using React, Next.js.</p>
           </div>
 
           <div className={styles.serviceCard}>
             <h3><FaCloudflare /> IOT SYSTEM & CLOUD FIREBASE</h3>
-            <p>Lorem ipsum dolor sit amet, tur adipisicing elit tempor.</p>
+            <p>Designing IoT and cloud integrated systems.</p>
           </div>
 
           <div className={styles.serviceCard}>
             <h3><FaChartLine /> SYSTEM ENGINEERING</h3>
-            <p>Lorem ipsum dolor sit amet, tur adipisicing elit tempor.</p>
+            <p>Server & network infrastructure maintenance and optimization.</p>
           </div>
 
           <div className={styles.serviceCard}>
             <h3><FaCode /> MACHINE LEARNING CNN</h3>
-            <p>Lorem ipsum dolor sit amet, tur adipisicing elit tempor.</p>
+            <p>Building systems with algorithm integration and Machine Learning.</p>
           </div>
 
         </div>
@@ -85,12 +85,17 @@ export default function Home() {
             <div className={styles.resumeItem}>
               <h4>Bachelor Degree</h4>
               <span>2021 - 2025 | Sumatera Institute of Technology</span>
-              <p>Lorem ipsum dolor sit amet, tur adipisicing elit tempor.</p>
+              <ul>
+                <li>Informatics Engineering Study Program</li>
+                <li>GPA 3.34</li>
+              </ul>
             </div>
             <div className={styles.resumeItem}>
               <h4>High School</h4>
               <span>2018 - 2021 | SMA Negeri 1 Telukdalam</span>
-              <p>Lorem ipsum dolor sit amet, tur adipisicing elit tempor.</p>
+              <ul>
+                <li>Mathematics & Natural Sciences</li>
+              </ul>
             </div>
           </div>
           
@@ -100,12 +105,13 @@ export default function Home() {
             <div className={styles.resumeItem}>
               <h4>System Engineering</h4>
               <span>2025 - Sekarang | PT Abdi Jaya Integra</span>
-              <p>Lorem ipsum dolor sit amet, tur adipisicing elit tempor.</p>
+              <p>
+Design, implement and maintain IT and Network infrastructure, and provide technical support to ensure systems run optimally and according to requirements.</p>
             </div>
             <div className={styles.resumeItem}>
               <h4>IT Support</h4>
               <span>July 2024 - August 2024 | PT PLN (Persero) UPP SUMBAGUT 3</span>
-              <p>Lorem ipsum dolor sit amet, tur adipisicing elit tempor.</p>
+              <p>Responsible for handling several LAN network issues, installing and configuring local networks, and coordinating with the team in network maintenance.</p>
             </div>
           </div>
         </div>
@@ -124,24 +130,24 @@ export default function Home() {
             </div>
           </div>
           <div className={styles.projectCard}>
-            <img src="/images/MonitoringCuaca.jpg" alt="Project 2" />
+            <img src="/images/GrafikML.png" alt="Project 2" />
+            <div className={styles.projectOverlay}>
+              <h3>Digital Image Classification</h3>
+              <span>Machine Learning</span>
+            </div>
+          </div>
+          <div className={styles.projectCard}>
+            <img src="/images/MobileApp.jpg" alt="Project 3" />
+            <div className={styles.projectOverlay}>
+              <h3>GameGratis Mobile App</h3>
+              <span>Mobile App</span>
+            </div>
+          </div>
+          <div className={styles.projectCard}>
+            <img src="/images/MonitoringCuaca.jpg" alt="Project 4" />
             <div className={styles.projectOverlay}>
               <h3>Purwarupa Monitoring Cuaca</h3>
               <span>Iot Development</span>
-            </div>
-          </div>
-          <div className={styles.projectCard}>
-            <img src="/placeholder3.jpg" alt="Project 3" />
-            <div className={styles.projectOverlay}>
-              <h3>Project Title 3</h3>
-              <span>UI/UX Design</span>
-            </div>
-          </div>
-          <div className={styles.projectCard}>
-            <img src="/placeholder4.jpg" alt="Project 4" />
-            <div className={styles.projectOverlay}>
-              <h3>Project Title 4</h3>
-              <span>Mobile App</span>
             </div>
           </div>
         </div>
@@ -157,7 +163,7 @@ export default function Home() {
               <span>10 Oct, 2025 | Web Design</span>
               <h3>How to Build a Sticky Sidebar</h3>
               <p>Lorem ipsum dolor sit amet, tur adipisicing elit tempor.</p>
-              <a href="#">Read More</a>
+              <a href="#blog">Read More</a>
             </div>
           </div>
           <div className={styles.blogCard}>
@@ -165,9 +171,19 @@ export default function Home() {
             <div className={styles.blogContent}>
               <span>26 Nov, 2025 | Icons Style</span>
               <h3>Most icons style with nodeJS</h3>
-              <p>Lorem ipsum dolor sit amet, tur adipisicing elit tempor.</p>
-              <p>VISIT THE ICON <a href="https://fontawesome.com/icons/dev?s="><FaIcons/></a></p>
-              <a href="#">Read More</a>
+              <p>VISIT THE ICONS
+              {/* target="_blank" → membuka link di tab baru
+               rel="noopener noreferrer" → keamanan tambahan agar tab baru tidak bisa mengakses window asal */}
+                <a
+                  className={styles.iconLink} 
+                  href="https://fontawesome.com/icons/dev?s=" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  >
+                  <FaIcons />
+                </a> 
+              </p>
+              <a href="#blog">Read M  ore</a>
             </div>
           </div>
         </div>
